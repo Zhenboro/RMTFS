a9ad6f12a3395e85faa89e5c658fe14c5514b9fb
Zhenboro <zhenboro@outlook.com>
2022-03-19 01:59:42 -0300

    init commit

---
+++ Cliente/Client.vb
@@ -0,0 +1,119 @@
+﻿Imports System.Net.Sockets
+Imports System.Runtime.Serialization.Formatters.Binary
+Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Threading
+Public Class Client
+    Dim YO As New TcpClient
+    Dim NS As NetworkStream
+    Dim ServerIP As String ' = "localhost"
+    Dim ServerPort As Integer ' = 190322
+
+    Dim RECIBE As Thread
+    Dim ENVIA As Thread
+    Public ENVIO As Byte()
+
+    Private Sub Client_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+        LeerParametros()
+        Iniciar()
+    End Sub
+    Private Sub Client_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Try
+            ENVIO = System.Text.Encoding.UTF7.GetBytes("END")
+            NS.Dispose()
+            YO.Close()
+        Catch
+        End Try
+    End Sub
+
+    Sub LeerParametros()
+        Try
+            If My.Application.CommandLineArgs.Count = 0 Then
+                End
+            Else
+                For i As Integer = 0 To My.Application.CommandLineArgs.Count - 1
+                    Dim parameter As String = My.Application.CommandLineArgs(i)
+                    If parameter Like "*-ServerIP=*" Then
+                        Dim args As String() = parameter.Split("=")
+                        ServerIP = args(1)
+                    ElseIf parameter Like "*-ServerPort=*" Then
+                        Dim args As String() = parameter.Split("=")
+                        ServerPort = Integer.Parse(args(1))
+                    End If
+                Next
+            End If
+        Catch ex As Exception
+            Console.WriteLine("LeerParametros Error: " & ex.Message)
+            End
+        End Try
+    End Sub
+
+    Sub Iniciar()
+        Try
+            YO.Connect(ServerIP, ServerPort)
+            RECIBE = New Thread(AddressOf RECIBIR)
+            RECIBE.Start()
+            ENVIA = New Thread(AddressOf ENVIAR)
+            ENVIA.Start()
+            Procesar("C:\")
+        Catch ex As Exception
+            Console.WriteLine("Iniciar Error: " & ex.Message)
+            End
+        End Try
+    End Sub
+
+    Sub RECIBIR()
+        Try
+            While True
+                NS = YO.GetStream
+            Dim BF As New BinaryFormatter
+                If NS.DataAvailable Then
+                    Procesar(System.Text.Encoding.UTF7.GetString(BF.Deserialize(NS)))
+                End If
+            End While
+        Catch ex As Exception
+            Console.WriteLine("RECIBIR Error: " & ex.Message)
+        End Try
+    End Sub
+    Sub ENVIAR()
+        Try
+            While True
+                If ENVIO IsNot Nothing Then
+                    Dim BF As New BinaryFormatter
+                    NS = YO.GetStream
+                    BF.Serialize(NS, ENVIO)
+                    ENVIO = Nothing
+                End If
+            End While
+        Catch ex As Exception
+            Console.WriteLine("ENVIAR Error: " & ex.Message)
+        End Try
+    End Sub
+    Sub Procesar(ByVal contenido As String)
+        Try
+            If contenido = "END" Then
+                End
+            Else
+                If contenido = "C:" Then
+                    contenido = "C:\"
+                End If
+                Dim folders As String = Nothing
+                Dim files As String = Nothing
+
+                For Each folder As String In My.Computer.FileSystem.GetDirectories(contenido, FileIO.SearchOption.SearchTopLevelOnly)
+                    folders &= folder & "|"
+                Next
+
+                For Each file As String In My.Computer.FileSystem.GetFiles(contenido, FileIO.SearchOption.SearchTopLevelOnly)
+                    files &= file & "|"
+                Next
+
+                ENVIO = System.Text.Encoding.UTF7.GetBytes(folders & files)
+            End If
+            Console.WriteLine("[Procesar] " & contenido)
+        Catch ex As Exception
+            Console.WriteLine("Procesar Error: " & ex.Message)
+        End Try
+    End Sub
+End Class
---
+++ Servidor/Server.vb
@@ -0,0 +1,114 @@
+﻿Imports System.Net.Sockets
+Imports System.Threading
+Imports System.Net
+Imports System.Runtime.Serialization.Formatters.Binary
+Public Class Server
+    Dim YO As TcpListener
+    Dim REMOTO As TcpClient
+    Dim RECIBE As Thread
+    Dim NS As NetworkStream
+    Dim ENVIA As Thread
+    Public ENVIO As Byte()
+    Dim DirArray As New ArrayList
+
+    Private Sub Server_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+    End Sub
+    Private Sub Server_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Try
+            NS.Dispose()
+            YO.Stop()
+            RECIBE.Abort()
+        Catch
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Iniciar()
+    End Sub
+
+    Sub Iniciar()
+        Try
+            Button1.Enabled = False
+            TextBox1.Enabled = False
+            Dim args As String() = TextBox1.Text.Split(":")
+            YO = New TcpListener(IPAddress.Any, args(1))
+            YO.Start()
+            RECIBE = New Thread(AddressOf RECIBIR)
+            ENVIA = New Thread(AddressOf ENVIAR)
+            RECIBE.Start()
+            ENVIA.Start()
+        Catch ex As Exception
+            Console.WriteLine("Iniciar Error: " & ex.Message)
+        End Try
+    End Sub
+    Sub RECIBIR()
+        Dim BF As New BinaryFormatter
+        Try
+            While True
+                REMOTO = YO.AcceptTcpClient()
+                NS = REMOTO.GetStream
+                While REMOTO.Connected = True
+                    Procesar(System.Text.Encoding.UTF7.GetString(BF.Deserialize(NS)))
+                End While
+            End While
+        Catch ex As Exception
+            Console.WriteLine("RECIBIR Error: " & ex.Message)
+            ENVIO = System.Text.Encoding.UTF7.GetBytes("END")
+        End Try
+    End Sub
+    Sub ENVIAR()
+        Try
+            While True
+                If ENVIO IsNot Nothing Then
+                    Dim BF As New BinaryFormatter
+                    NS = REMOTO.GetStream
+                    BF.Serialize(NS, ENVIO)
+                    ENVIO = Nothing
+                End If
+            End While
+        Catch ex As Exception
+            Console.WriteLine("ENVIAR Error: " & ex.Message)
+        End Try
+    End Sub
+    Sub Procesar(ByVal contenido As String)
+        Try
+            If contenido = "END" Then
+                End
+            Else
+                DirArray.Clear()
+                ListBox1.Items.Clear()
+                DirArray.Add("..")
+                ListBox1.Items.Add("..")
+                For Each item As String In contenido.Split("|")
+                    DirArray.Add(item)
+                    ListBox1.Items.Add(IO.Path.GetFileName(item))
+                Next
+                ListBox1.Enabled = True
+            End If
+            Console.WriteLine("[Procesar] " & contenido)
+        Catch ex As Exception
+            Console.WriteLine("Procesar Error: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub ListBox1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles ListBox1.MouseDoubleClick
+        If ListBox1.SelectedItem = ".." Then
+            'C:\Users\Zhenboro\source\repos\RMTFS\Servidor\bin\Debug
+            Dim i = TextBox2.Text.LastIndexOf("\") 'Find the index to cut
+            Dim rutaMenor = TextBox2.Text.Substring(0, i)
+            TextBox2.Text = rutaMenor
+            ENVIO = System.Text.Encoding.UTF7.GetBytes(rutaMenor)
+        Else
+            ENVIO = System.Text.Encoding.UTF7.GetBytes(DirArray(ListBox1.SelectedIndex))
+            TextBox2.Text = DirArray(ListBox1.SelectedIndex)
+        End If
+        ListBox1.Enabled = False
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If TextBox2.Text = "C:" Then
+            TextBox2.Text = "C:\"
+        End If
+    End Sub
+End Class
